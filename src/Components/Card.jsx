c5c9b68a775570ae63402914d270cd5743874f0a
KadonWills <kapolw@gmail.com>
2023-05-12 09:53:29 +0100

    Design card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({children}) => {
+    return (
+        <div
+            className="w-full flex flex-row justify-center relative rounded-xl p-5 md:p-3  border-0 bg-white  "
+            style={{ boxShadow: '0px 0px 18px #00000026' }} >
+
+            { children }
+        </div>
+    )
+}
+
+export default Card
