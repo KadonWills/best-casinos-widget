fdd7598a2092ac454f1851b9e0a8a8059ab889e1
KadonWills <kapolw@gmail.com>
2023-05-12 09:59:54 +0100

    Fixed lg card padding issue

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({children}) => {
     return (
         <div
-            className="w-full flex flex-row justify-center relative rounded-xl p-5 md:p-3  border-0 bg-white  "
+            className="w-full flex flex-row justify-center relative rounded-xl p-5 md:p-5  border-0 bg-white  "
             style={{ boxShadow: '0px 0px 18px #00000026' }} >
 
             { children }
--- src/Components/LogoWithRatings.jsx
+++ src/Components/LogoWithRatings.jsx
@@ -8,7 +8,7 @@ const LogoWithRatings = () => {
 
     return (
         <Row gutter={10} className=' md:flex md:flex-row md:col-span-1 lg:col-span-2    justify-between md:justify-start lg:justify-between   md:items-center'>
-            <Col className="absolute right-3  font-sans top-1 text-2xl text-[#FF5762] font-bold md:relative max-w-min pl-2 lg:px-5 lg:w-[50px]">01</Col>
+            <Col className="absolute right-3  font-sans top-1 text-2xl text-[#FF5762] font-bold md:relative max-w-max pl-2 lg:px-5 lg:w-[50px]">01</Col>
             <Col span={12} className='lg:max-w-min'>
 
                 <div className="w-[124px] h-[124px] md:w-[170px] md:h-[170px] lg:w-[124px] lg:h-[124px] rounded-full bg-[#6a45a5] relative">
